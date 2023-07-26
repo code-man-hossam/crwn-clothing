8f1a3affe858d3b93f18c4479bd5fb343a83c8a3
code-man-hossam <hossamabualgassimg@gmail.com>
2023-07-26 02:21:09 -0700

    initalizing crwn clothing

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+
+const App = () => {
+  return (
+    <div className="categories-container">
+      <div className="category-container">
+        hello
+      </div>
+    </div>
+  );
+}
+
+export default App;
